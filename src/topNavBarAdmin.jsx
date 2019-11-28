57afa3ca36d7a8312b43a64ada242a49806f4eae
Krzysztof Nadlonek <knadlonek@gmail.com>
2019-11-28 12:56:39 +0100

    created top navbar for Admin and Manager

---
+++ src/topNavBarAdmin.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Container, Columns,Column} from 'react-bulma-components';
+import Profilepicture from './assets/profilepicture.jpg'
+
+
+
+const TopNavBarAdmin = () => (
+  <>
+    <Container>
+      <nav
+        class="navbar topNavBar"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/home">
+            TrackR
+          </a>
+          <a class="navbar-item" href="/home">
+            <img
+              src={Profilepicture}
+              class="profilepicture"
+              alt="profilepicture"
+            />
+          </a>
+
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">HOME</a>
+            <a class="navbar-item">USER LIST</a>
+          </div>
+          
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button" id="buttonWhite">LOG OUT</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </Container>
+  </>
+);
+
+
+  export default TopNavBarAdmin;
---
+++ src/topNavBarManager.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Container, Columns,Column} from 'react-bulma-components';
+import Profilepicture from './assets/profilepicture.jpg'
+
+
+
+const TopNavBarManager = () => (
+  <>
+    <Container>
+      <nav
+        class="navbar topNavBar"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/home">
+            TrackR
+          </a>
+          <a class="navbar-item" href="/home">
+            <img
+              src={Profilepicture}
+              class="profilepicture"
+              alt="profilepicture"
+            />
+          </a>
+
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">HOME</a>
+            <a class="navbar-item">STUDENTS LIST</a>
+            <a class="navbar-item">STUDENTS KPI</a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button" id="buttonWhite">MY INFO</a>
+              </div>
+            </div>
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button" id="buttonWhite">LOG OUT</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </Container>
+  </>
+);
+
+
+  export default TopNavBarManager;
