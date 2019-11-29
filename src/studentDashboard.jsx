89fb7ca573f9d0fdcb79d2483cae5c7dd37fb874
Krzysztof Nadlonek <knadlonek@gmail.com>
2019-11-29 11:36:34 +0100

    moves files

--- src/studentDashboard.jsx
+++ src/studentDashboard.jsx
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream:src/studentDashboard.jsx
 import React from 'react';
 import TopNavBarStudent from './topNavBarStudent';
+=======
+import React , {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import  {getCurrentProfile} from '../actions/profile';
+import TopNavBarStudent from './navbars/topNavBarStudent';
+>>>>>>> Stashed changes:src/components/studentDashboard.jsx
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Box,Container, Columns,Column} from 'react-bulma-components'
 
