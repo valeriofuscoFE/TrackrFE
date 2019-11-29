5108326bc6726b0f5662a4c2f81188a7e94c5f7b
Krzysztof Nadlonek <knadlonek@gmail.com>
2019-11-29 11:47:28 +0100

    moved files and modified redux store

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import {Container, Columns,Column} from 'react-bulma-components'
 import './styles/style.scss';
-import TopNavBar from './topNavBar';
+import TopNavBar from './components/navbars/topNavBar';
 import SideNavBar from './sideNavBar';
 import LandingPage from './landingPage';
 import ManagerDashboard from './managerDashboard';
-import StudentDashboard from './studentDashboard';
+import StudentDashboard from './components/studentDashboard';
 import StudentMyInfo from './studentMyInfo';
 import ManagerMyInfo from './managerMyInfo';
 import AddStudent from './addStudent';
@@ -22,37 +22,70 @@ import StudentsList from './studentsList';
 import Test from './test';
 import UserList from './userList';
 import StudentsKPI from './studentsKPI';
+//REDUX
+import {Provider} from 'react-redux'
+import store from './store'
+import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute'
 
 
 
 
-const App = () => (
-  <>
-    <Container>
-       {/* <ManagerDashboard/> */}
-       {/* <AddStudent/> */}
-       {/* <StudentsList/> */}
-       <StudentsKPI/>
-       {/* <UserList/> */}
-       {/* <SchoolDetails/> */}
-       {/* <AddJobApplication/> */}
-       {/* <Test/> */}
-       {/* <CompanyInfoModal/> */}
-       {/* <JobInfoModal/> */}
-       {/* <TasksModal/> */}
-       {/* <OfferModal/> */}
-{/*     
-      <StudentDashboard/> */}
-       {/* <ManagerMyInfo/> */}
-       {/* <StudentMyInfo/> */}
-      {/* <AdminDashboard/> */}
-      {/* <TopNavBar /> */}
-      {/* <LandingPage /> */}
-      {/* <SideNavBar /> */}
-      <Footer/>
-    </Container>
-  </>
-);
+if(localStorage.token){
+	setAuthToken(localStorage.token);
+}
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, [])  //WE HAVE BRACKETS BECAUSE WE WANT THAT WE WILL RUN ONLY ONCE 
+
+	return (
+		<Provider store={store}>
+		<Router>
+			<Fragment>
+				<NavBar/>
+				<Route exact path ='/' component = {Landing}/>
+				<section className="container">
+					<Switch>
+						<Route exact path ='/register' component={Register}/>
+						<Route exact path ='/login' component={Login}/>
+						<PrivateRoute exact path ='/dashboard' component={Dashboard}/>
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+		</Provider>
+		  );
+}
+
+
+// const App = () => (
+//   <>
+//     <Container>
+//        {/* <ManagerDashboard/> */}
+//        {/* <AddStudent/> */}
+//        {/* <StudentsList/> */}
+//        <StudentsKPI/>
+//        {/* <UserList/> */}
+//        {/* <SchoolDetails/> */}
+//        {/* <AddJobApplication/> */}
+//        {/* <Test/> */}
+//        {/* <CompanyInfoModal/> */}
+//        {/* <JobInfoModal/> */}
+//        {/* <TasksModal/> */}
+//        {/* <OfferModal/> */}
+// {/*     
+//       <StudentDashboard/> */}
+//        {/* <ManagerMyInfo/> */}
+//        {/* <StudentMyInfo/> */}
+//       {/* <AdminDashboard/> */}
+//       {/* <TopNavBar /> */}
+//       {/* <LandingPage /> */}
+//       {/* <SideNavBar /> */}
+//       <Footer/>
+//     </Container>
+//   </>
+// );
 
 
   export default App;
--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
 import {createStore,applyMiddleware} from 'redux';
-import rootReducer from './reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
 const initialState = {};
 
 const middleware = [thunk]
 
-const store = createStore(
-	rootReducer,
-	initialState,
-	applyMiddleware(...middleware));
+const store = createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
 
-
-export default  store ;
+export default store;
