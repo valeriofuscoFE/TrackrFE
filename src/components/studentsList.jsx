3e595689b6637cef6c98612349d79c58c65749a0
Krzysztof Nadlonek <knadlonek@gmail.com>
2019-12-05 18:00:00 +0100

    modified redux and added searchbar for student and user lists

--- src/components/studentsList.jsx
+++ src/components/studentsList.jsx
@@ -5,7 +5,6 @@ import { Box,Container, Columns,Column} from 'react-bulma-components';
 import {fetchUsers} from '../actions/usersActions';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import StudentsFilter from "./studentsFilter";
 
 
 const StudentsList = ({fetchUsers,usersReducer}) => {
@@ -16,54 +15,6 @@ useEffect(()=>{
 },[])
 
 
-
-//filter
-
-// function filter(){
-//   var input, filter , ul,li,a,i;
-
-//   input = document.getElementById('value');
-//   filter = input.value.toUpperCase();
-
-//   ul = document.getElementById('wrapper');
-//   li = document.getElementById('name');
-
-//   for (i = 0; i < li.length; i++) {
-//     a = li[i].getElementsByID("a")[0];
-//     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//         li[i].style.display = "";
-//     } else {
-//         li[i].style.display = "none";
-//     }
-// }
-// }
-// console.log(filter)
-
-
-// const [search, filterSearch] = useState()
-
-//  const  searchHandler = (e) => {
-//   filterSearch({...search, search: e.target.value});
-//   console.log("searchHandler submitted", search);
-// };
-
-
-// const searchStudents = usersReducer.map(student=> {
-
-//   if (student.name === search)
-//   return(
-//   <div id="myTable">
-//    <div  key={student._id} class="columns is-centered" >
-//    <div id="name" class="column is-2 topColumn"><a id="a">{student.name}</a></div>
-//    <div id="name" class="column is-2 topColumn">{student.surname}</div>
-//  </div>
-//  <hr></hr>
-//  </div>
-
-
-//   );
-// })
-
 const studentsList = usersReducer.map(student=>(
   <div>
    <div  key={student._id} class="columns is-gapless is-centered" >
@@ -72,6 +23,9 @@ const studentsList = usersReducer.map(student=>(
    <div  class="column is-3 topColumn">{student.email}</div>
    <div  class="column is-3 topColumn">{student.githubUrl}</div>
    <div  class="column is-2 topColumn">{student.school}</div>
+   <button class="button" id="buttonDelete">
+         X
+   </button>
  </div>
  <hr></hr>
  </div>
@@ -79,32 +33,32 @@ const studentsList = usersReducer.map(student=>(
 ))
 
 const [searchStudent, setSearchStudent] = useState("");
-const [searchResult, setSearchResult] = useState(usersReducer);
-// const [searchResult, setSearchResult] = useState([]);
+const [searchResult, setSearchResult] = useState([]);
 
 const searchHandler = e =>{
   setSearchStudent(e.target.value);
 }
-console.log(searchStudent)
 
 useEffect(() => {
   const results = usersReducer.filter(student=>
-    student.toString().includes(searchStudent)
+    student.name.toString().toLowerCase().includes(searchStudent.toLowerCase())
   );
-  setSearchResult(results);
+
+  if (searchStudent.length > 2)
+  return (
+  setSearchResult(results));
 }, [searchStudent]);
 
-console.log(searchResult)
+console.log("searchResult",searchResult)
+
+
 
 
   // const searchResult = !searchStudent
   // ? usersReducer
   // : usersReducer.filter(student =>
-  //     student.toString().toLowerCase().includes(searchStudent.toLocaleLowerCase())
+  //     student.toString().toLowerCase().includes(searchStudent.toLowerCase())
   //   );
-
-
-// console.log("studentList",usersReducer)
 
   return (
     <>
@@ -132,25 +86,8 @@ console.log(searchResult)
             </button>
           </div>
         </div>
+   
         {/* SECOND ROW */}
-
-        <div>
-          {searchResult.map(search => (
-            <div key={search._id}>{search}</div>
-          ))}
-        </div>
-        
-
-
-        <div>
-          {/* {searchStudents} */}
-          {/* <StudentsFilter
-          usersReducer={usersReducer.filter(
-            student =>
-              student.name
-          )}/> */}
-        </div>
-
         <div class="columns is-centered">
           <div class="column topColumn">Name</div>
           <div class="column topColumn">Surname</div>
@@ -160,6 +97,26 @@ console.log(searchResult)
         </div>
         <hr></hr>
 
+             {/* SEARCH MAPPING*/}
+
+             <div>
+          {searchResult.map(search => (
+              <div>
+              <div  key={search._id} class="columns is-gapless is-centered" >
+              <div  class="column is-2 filteredColumn">{search.name}</div>
+              <div  class="column is-2 filteredColumn">{search.surname}</div>
+              <div  class="column is-3 filteredColumn">{search.email}</div>
+              <div  class="column is-3 filteredColumn">{search.githubUrl}</div>
+              <div  class="column is-2 filteredColumn">{search.school}</div>
+              <button class="button" id="buttonDelete">
+                    X
+              </button>
+            </div>
+            <hr></hr>
+            </div>
+          ))}
+        </div>
+      {/* STUDENT LIST */}  
         <div>{studentsList}</div>
       </Container>
     </>
